a4783c6d0b6960ab0b84e46e8ec31f4c1647a005
kajal-2011 <kajalrekha888@gmail.com>
2023-07-16 01:15:05 +0600

    done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
-    <header className="h-20 fixed top-0 left-0 right-0 bg-red-500">
-      Navbar
+    <header className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border-b border-dark/10 z-[100]">
+      <div className="wrapper w-full h-full"></div>
     </header>
   );
 };
