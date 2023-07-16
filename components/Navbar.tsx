2d53fc32e435c30851b3e4d3505b2b6087f1e09c
kajal-2011 <kajalrekha888@gmail.com>
2023-07-16 15:41:08 +0600

    navbar added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,46 @@
+import Link from "next/link";
+import Button from "./Button";
+
 const Navbar = () => {
   return (
     <header className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border-b border-dark/10 z-[100]">
-      <div className="wrapper w-full h-full"></div>
+      <div className="wrapper w-full h-full flex justify-between items-center">
+        {/* NAV LEFT */}
+        <nav>
+          <Link href="/" className="text-xl font-semibold link-item">
+            Dazzle Mart
+          </Link>
+        </nav>
+        {/* NAV MID */}
+        <nav>
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/" className="link-item">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/products" className="link-item">
+                Products
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className="link-item">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="link-item">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* NAV RIGHT */}
+        <nav>
+          <Button href="/login" placeholder="Login" color="green" />
+        </nav>
+      </div>
     </header>
   );
 };
